8263eaa979c62a189b02ad0601a146d6870eca12
liakassardjian <liakassardjian@gmail.com>
2019-10-31 17:11:33 -0300

    Criação da classe Rule

---
+++ GameOfLife/GameOfLife/Model/Rule.swift
@@ -0,0 +1,13 @@
+//
+//  Rule.swift
+//  GameOfLife
+//
+//  Created by Lia Kassardjian on 31/10/19.
+//  Copyright © 2019 Lia Kassardjian. All rights reserved.
+//
+
+import Foundation
+
+class Rule {
+    
+}
